4bf75522114cd9776b621e97272ef2126b84c070
zach-morris-txt <sbpayetc@gmail.com>
2024-12-01 19:14:13 -0500

    Clean React bloat, develop simple app /home

--- dnd-tools/src/App.js
+++ dnd-tools/src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <header>D&D Tools</header>
+      <div className="homeCol">
+        <div className="leftCol">LEFT SPACE</div>
+        <div className="rightCol">RIGHT SPACE
+          <div className="charShtTop">
+            <input type="text" className="shtInputName" />
+            <div>
+            <input type="text" placeholder="shtInputClass" className="shtInputClass" />
+            <input type="text" placeholder="shtInputBackground" className="shtInputBackground" />
+            <input type="text" placeholder="shtInputPlayerName" className="shtInputPlayerName" />
+            <input type="text" placeholder="shtInputRace" className="shtInputRace" />
+            <input type="text" placeholder="shtInputAlignment" className="shtInputAlignment" />
+            <input type="text" placeholder="shtInputXP" className="shtInputXP" />
+            </div>
+          </div>
+          <div className="charShtCols">
+            <div className="charShtColLeft"></div>
+            <div className="charShtColMid"></div>
+            <div className="charShtColRight"></div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
